44392e833f19e324a7dc3deff833942bb10a5d44
lucas lamar <llamareo@gmail.com>
2023-11-17 12:19:14 -0600

    fixed files other than idex not loading in the vercel hosting. had to add a vite.config.js files

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,16 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "src/lesson_html/concepts_of_OOP.html"),
+        concepts_of_OOP: resolve(
+          __dirname,
+          "src/lesson_html/concepts_of_OOP.html"
+        ),
+        arrays: resolve(__dirname, "src/lesson_html/arrays.html"),
+        countsort: resolve(__dirname, "src/lesson_html/countsort.html"),
+        linked_list: resolve(__dirname, "src/lesson_html/linked_list.html"),
+        mergesort: resolve(__dirname, "src/lesson_html/mergesort.html"),
+        selsort: resolve(__dirname, "src/lesson_html/selsort.html"),
+        about_us: resolve(__dirname, "src/lesson_html/about_us.html"),
       },
     },
   },
